f5f596f2743a497347b593d68af92ac53c46ff3e
Mukhsinjon Bokijonov <78298971+BokijonovM@users.noreply.github.com>
2022-03-16 21:21:14 +0500

    updated

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,7 +1,30 @@
-import React from "react";
+import { async } from "q";
+import React, { useEffect, useState } from "react";
 import MyNavbar from "./MyNavbar";
 
 function Main() {
+  const [blogs, setBlogs] = useState([]);
+  const fetchData = async () => {
+    try {
+      let res = await fetch("http://localhost:3001/user/me/stories", {
+        method: "GET",
+        headers: {},
+      });
+      if (res.ok) {
+        let data = await res.json();
+        setBlogs(data);
+        console.log(data);
+      } else {
+        console.log("fetch error");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
   return (
     <div>
       <MyNavbar />
